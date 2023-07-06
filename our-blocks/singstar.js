2014a32d0a8f56ba79005a94c07a7eb90bbbb4c4
thetaconsulting <terhivesanen@gmail.com>
2023-07-06 14:43:35 +0300

    clean-up

--- our-blocks/singstar.js
+++ our-blocks/singstar.js
@@ -1,7 +1,7 @@
 wp.blocks.registerBlockType("ourdatabaseplugin/singstar", {
   title: "Singstar catalogue",
   edit: function () {
-    return wp.element.createElement("div", { className: "our-placeholder-block" }, "Pets List Placeholder")
+    return wp.element.createElement("div", { className: "our-placeholder-block" }, "Singstar catalogue Placeholder")
   },
   save: function () {
     return null
